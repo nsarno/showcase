21ce76932851ea849e25607848cb236c037bd688
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-06-07 22:07:00 +0200

    add react router

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,16 @@
 var React = require('react');
 var DocumentTitle = require('react-document-title');
 
-var Home = React.createClass({
+var Contact = React.createClass({
   render: function() {
     return (
       <div className="container">
-        <DocumentTitle title='Home'>
-          <h1>Home</h1>
+        <DocumentTitle title='Contact'>
+          <h1>Contact</h1>
         </DocumentTitle>
       </div>
     );
   }
 });
 
-module.exports = Home;
+module.exports = Contact;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,33 @@
 var React = require('react');
-var Home = require('Home');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
 
-React.render(
-  <Home />,
-  document.getElementById('app')
+var Navbar = require('Navbar');
+var Home = require('Home');
+var Contact = require('Contact');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Navbar />
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name='app' path='/' handler={App}>
+    <DefaultRoute handler={Home}/>
+    <Route name='contact' handler={Contact}/>
+  </Route>
+
 );
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
